ed0f1ff9315f24ab21f809106f537ef7b60665c2
Adriano Emerick Cola <adrianocola@gmail.com>
2015-04-10 22:27:16 -0300

    Atualizado jquery-ui (utilizando só módulos necessários)

--- public/scripts/basketball/startup.js
+++ public/scripts/basketball/startup.js
@@ -74,7 +74,7 @@ define([
     $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
 
     //configura o moment.js para os padrões brasileiros
-    moment.lang('pt-br', {
+    moment.locale('pt-br', {
         months : "Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro".split("_"),
         monthsShort : "Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez".split("_"),
         weekdays : "Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado".split("_"),
